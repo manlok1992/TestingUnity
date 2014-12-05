0a1313b6f3b0cdd1e41282f3c7d959667a20d59f
manlok1992 <manlok@Mans-Mac-mini-2.local>
2014-12-05 11:06:12 +0800

    backup

--- New Unity Project 5/Assets/testSocket.cs
+++ New Unity Project 5/Assets/testSocket.cs
@@ -6,6 +6,7 @@ public class testSocket : MonoBehaviour {
 	// Use this for initialization
 	void Start() {
 		StartCoroutine(waiting());
+		PlayerPrefs.SetInt("123", 1);
 	}
 
 	IEnumerator waiting() {
