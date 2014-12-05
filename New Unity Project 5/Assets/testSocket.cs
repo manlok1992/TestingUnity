13de5e7e66f3782a498db48983d4ecbb2d41ea21
manlok1992 <manlok@Mans-Mac-mini-2.local>
2014-12-05 15:28:43 +0800

    updated

--- New Unity Project 5/Assets/testSocket.cs
+++ New Unity Project 5/Assets/testSocket.cs
@@ -3,7 +3,9 @@ using System.Collections;
 using System.IO;
 using System;
 public class testSocket : MonoBehaviour {
+	string arr;
 	// Use this for initialization
+	SimpleJSON.JSONNode node;
 	void Start() {
 		StartCoroutine(waiting());
 		PlayerPrefs.SetInt("123", 1);
@@ -12,7 +14,7 @@ public class testSocket : MonoBehaviour {
 	IEnumerator waiting() {
 			// We should only read the screen after all rendering is complete
 		yield return new WaitForEndOfFrame();
-		// Create a texture the size of the screen, RGB24 format
+//		// Create a texture the size of the screen, RGB24 format
 		int width = Screen.width;
 		int height = Screen.height;
 		Texture2D tex = new Texture2D( width, height, TextureFormat.RGB24, false );
@@ -25,21 +27,35 @@ public class testSocket : MonoBehaviour {
 //		// Create a Web Form
 		WWWForm form = new WWWForm();
 		form.AddField("frameCount", Time.frameCount.ToString());
-		form.AddBinaryData("fileUpload", bytes, "screenShot.png", "image/png");
+		form.AddBinaryData("fileUpload", bytes, "screenShot.jpg", "image/jpg");
 		// Upload to a cgi script
 		form.AddField("test", 1234324);
 		form.AddField("test2", 1234);
-		WWW w = new WWW("http://127.0.0.1:1111", form);
+		WWW w = new WWW("http://169.254.31.4:1111", form);
 		yield return w;
 		if (w.error != null)
 			Debug.Log("Error: "+w.error);
-		else
-			Debug.Log ("get data: "+w.text);
+		else {
+			arr = w.text;
+			Debug.Log (w.text);
+			var n = SimpleJSON.JSONNode.Parse(w.text);
+			Debug.Log (n);
+			Debug.Log (n.Count);
+			for(int i = 0; i < n.Count; i++) {
+				string temp = n[i]["id"].ToString();
+				string temp1 = n[i]["name"].ToString();
+				string temp2 = n[i]["message"].ToString();
+				Debug.Log ("get data id: "+temp);
+				Debug.Log ("get data name: "+temp1);
+				Debug.Log ("get data message: "+temp2);
+			}
+
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		Debug.Log ("get: "+PlayerPrefs.GetInt("123"));
 	}
 }
 	
--- TestServer/routes/index.js
+++ TestServer/routes/index.js
@@ -10,11 +10,17 @@ router.get('/', function(req, res) {
 
 router.post('/', function(req, res) {
   console.log(req.body.test);
-  var user = {id: 1, name: "manlok", message: "JsonTesting"}
-  var userJson = JSON.stringify(user);
+  var user = {id: "1", name: "manlok", message: "JsonTesting"}
+  var user1 = {id: "2", name: "user2", message: "JsonTesting"}
+  // var userJson = JSON.stringify(user);
+  // var userJson1 = JSON.stringify(user1);
+  var temp = [];
+  temp.push(user);
+  temp.push(user1);
+  // temp.push("userJson");
   // var data = str2json.convert({id: 1, name: "manlok", message: "JsonTesting"});
-  res.send(userJson);
-  res.send("abd321");
+  res.send(temp);
+  // res.send("userJson");
 });
 
 module.exports = router;
